b15d4f6598a73c2251f8580587aaf22194512f50
Kim JinSeon <kwlstjs00@gmail.com>
2024-02-10 13:26:54 +0900

    fix: pharmacy dto type 변경
    운영시간 Integer -> String

--- src/main/java/ssg/middlepj/pharmafinder/dto/PharmacyDto.java
+++ src/main/java/ssg/middlepj/pharmafinder/dto/PharmacyDto.java
@@ -9,22 +9,22 @@ public class PharmacyDto {
     private String dutyAddr; // 주소
     private String dutyName; // 약국명
     private String dutyTel1; // 대표번호 1
-    private Integer dutyTime1c; // 진료시간(mon) Close
-    private Integer dutyTime1s; // 진료시간(mon) Start
-    private Integer dutyTime2c; // 진료시간(tue) Close
-    private Integer dutyTime2s; // 진료시간(tue) Start
-    private Integer dutyTime3c; // 진료시간(wed) Close
-    private Integer dutyTime3s; // 진료시간(wed) Start
-    private Integer dutyTime4c; // 진료시간(thu) Close
-    private Integer dutyTime4s; // 진료시간(thu) Start
-    private Integer dutyTime5c; // 진료시간(fri) Close
-    private Integer dutyTime5s; // 진료시간(fri) Start
-    private Integer dutyTime6c; // 진료시간(sat) Close
-    private Integer dutyTime6s; // 진료시간(sat) Start
-    private Integer dutyTime7c; // 진료시간(sun) Close
-    private Integer dutyTime7s; // 진료시간(sun) Start
-    private Integer dutyTime8c; // 진료시간(holi) Close
-    private Integer dutyTime8s; // 진료시간(holi) Start
+    private String dutyTime1c; // 진료시간(mon) Close
+    private String dutyTime1s; // 진료시간(mon) Start
+    private String dutyTime2c; // 진료시간(tue) Close
+    private String dutyTime2s; // 진료시간(tue) Start
+    private String dutyTime3c; // 진료시간(wed) Close
+    private String dutyTime3s; // 진료시간(wed) Start
+    private String dutyTime4c; // 진료시간(thu) Close
+    private String dutyTime4s; // 진료시간(thu) Start
+    private String dutyTime5c; // 진료시간(fri) Close
+    private String dutyTime5s; // 진료시간(fri) Start
+    private String dutyTime6c; // 진료시간(sat) Close
+    private String dutyTime6s; // 진료시간(sat) Start
+    private String dutyTime7c; // 진료시간(sun) Close
+    private String dutyTime7s; // 진료시간(sun) Start
+    private String dutyTime8c; // 진료시간(holi) Close
+    private String dutyTime8s; // 진료시간(holi) Start
     private String hpid; // 약국 ID
     private float wgs84Lon; // 경도
     private float wgs84Lat; // 위도
--- src/main/java/ssg/middlepj/pharmafinder/util/XMLParser.java
+++ src/main/java/ssg/middlepj/pharmafinder/util/XMLParser.java
@@ -52,102 +52,99 @@ public class XMLParser {
             }
 
             NodeList dutyTime1cList = ((Element) itemNode).getElementsByTagName("dutyTime1c");
-            if (dutyTime1cList.getLength() > 0) {
+            if (dutyTime1cList.getLength()>0) {
                 Node dutyTime1cNode = dutyTime1cList.item(0);
-                pharmacyDto.setDutyTime1c(Integer.parseInt(dutyTime1cNode.getTextContent().trim()));
+                pharmacyDto.setDutyTime1c(dutyTime1cNode.getTextContent().trim());
             }
 
             NodeList dutyTime1sList = ((Element) itemNode).getElementsByTagName("dutyTime1s");
             if (dutyTime1sList.getLength() > 0) {
                 Node dutyTime1sNode = dutyTime1sList.item(0);
-                pharmacyDto.setDutyTime1s(Integer.parseInt(dutyTime1sNode.getTextContent().trim()));
+                pharmacyDto.setDutyTime1s(dutyTime1sNode.getTextContent().trim());
             }
 
             NodeList dutyTime2cList = ((Element) itemNode).getElementsByTagName("dutyTime2c");
             if (dutyTime2cList.getLength() > 0) {
                 Node dutyTime2cNode = dutyTime2cList.item(0);
-                pharmacyDto.setDutyTime2c(Integer.parseInt(dutyTime2cNode.getTextContent().trim()));
+                pharmacyDto.setDutyTime2c(dutyTime2cNode.getTextContent().trim());
             }
 
             NodeList dutyTime2sList = ((Element) itemNode).getElementsByTagName("dutyTime2s");
             if (dutyTime2sList.getLength() > 0) {
                 Node dutyTime2sNode = dutyTime2sList.item(0);
-                pharmacyDto.setDutyTime2s(Integer.parseInt(dutyTime2sNode.getTextContent().trim()));
+                pharmacyDto.setDutyTime2s(dutyTime2sNode.getTextContent().trim());
             }
 
             NodeList dutyTime3cList = ((Element) itemNode).getElementsByTagName("dutyTime3c");
             if (dutyTime3cList.getLength() > 0) {
                 Node dutyTime3cNode = dutyTime3cList.item(0);
-                pharmacyDto.setDutyTime3c(Integer.parseInt(dutyTime3cNode.getTextContent().trim()));
+                pharmacyDto.setDutyTime3c(dutyTime3cNode.getTextContent().trim());
             }
 
             NodeList dutyTime3sList = ((Element) itemNode).getElementsByTagName("dutyTime3s");
             if (dutyTime3sList.getLength() > 0) {
                 Node dutyTime3sNode = dutyTime3sList.item(0);
-                pharmacyDto.setDutyTime3s(Integer.parseInt(dutyTime3sNode.getTextContent().trim()));
+                pharmacyDto.setDutyTime3s(dutyTime3sNode.getTextContent().trim());
             }
 
             NodeList dutyTime4cList = ((Element) itemNode).getElementsByTagName("dutyTime4c");
             if (dutyTime4cList.getLength() > 0) {
                 Node dutyTime4cNode = dutyTime4cList.item(0);
-                pharmacyDto.setDutyTime4c(Integer.parseInt(dutyTime4cNode.getTextContent().trim()));
+                pharmacyDto.setDutyTime4c(dutyTime4cNode.getTextContent().trim());
             }
 
             NodeList dutyTime4sList = ((Element) itemNode).getElementsByTagName("dutyTime4s");
             if (dutyTime4sList.getLength() > 0) {
                 Node dutyTime4sNode = dutyTime4sList.item(0);
-                pharmacyDto.setDutyTime4s(Integer.parseInt(dutyTime4sNode.getTextContent().trim()));
+                pharmacyDto.setDutyTime4s(dutyTime4sNode.getTextContent().trim());
             }
 
             NodeList dutyTime5cList = ((Element) itemNode).getElementsByTagName("dutyTime5c");
             if (dutyTime5cList.getLength() > 0) {
                 Node dutyTime5cNode = dutyTime5cList.item(0);
-                pharmacyDto.setDutyTime5c(Integer.parseInt(dutyTime5cNode.getTextContent().trim()));
+                pharmacyDto.setDutyTime5c(dutyTime5cNode.getTextContent().trim());
             }
 
             NodeList dutyTime5sList = ((Element) itemNode).getElementsByTagName("dutyTime5s");
             if (dutyTime5sList.getLength() > 0) {
                 Node dutyTime5sNode = dutyTime5sList.item(0);
-                pharmacyDto.setDutyTime5s(Integer.parseInt(dutyTime5sNode.getTextContent().trim()));
+                pharmacyDto.setDutyTime5s(dutyTime5sNode.getTextContent().trim());
             }
 
             NodeList dutyTime6cList = ((Element) itemNode).getElementsByTagName("dutyTime6c");
             if (dutyTime6cList.getLength() > 0) {
                 Node dutyTime6cNode = dutyTime6cList.item(0);
-                pharmacyDto.setDutyTime6c(Integer.parseInt(dutyTime6cNode.getTextContent().trim()));
+                pharmacyDto.setDutyTime6c(dutyTime6cNode.getTextContent().trim());
             }
 
             NodeList dutyTime6sList = ((Element) itemNode).getElementsByTagName("dutyTime6s");
             if (dutyTime6sList.getLength() > 0) {
                 Node dutyTime6sNode = dutyTime6sList.item(0);
-                pharmacyDto.setDutyTime6s(Integer.parseInt(dutyTime6sNode.getTextContent().trim()));
+                pharmacyDto.setDutyTime6s(dutyTime6sNode.getTextContent().trim());
             }
 
-            // 7,8이 있을때만 작동
-            if (dutyTime6sList.getLength() > 0) {
-                NodeList dutyTime7cList = ((Element) itemNode).getElementsByTagName("dutyTime7c");
-                if (dutyTime7cList.getLength() > 0) {
-                    Node dutyTime7cNode = dutyTime7cList.item(0);
-                    pharmacyDto.setDutyTime7c(null);
-                }
+            NodeList dutyTime7cList = ((Element) itemNode).getElementsByTagName("dutyTime7c");
+            if (dutyTime7cList.getLength() > 0) {
+                Node dutyTime7cNode = dutyTime7cList.item(0);
+                pharmacyDto.setDutyTime7c(dutyTime7cNode.getTextContent().trim());
+            }
 
-                NodeList dutyTime7sList = ((Element) itemNode).getElementsByTagName("dutyTime7s");
-                if (dutyTime7sList.getLength() > 0) {
-                    Node dutyTime7sNode = dutyTime7sList.item(0);
-                    pharmacyDto.setDutyTime7s(null);
-                }
+            NodeList dutyTime7sList = ((Element) itemNode).getElementsByTagName("dutyTime7s");
+            if (dutyTime7sList.getLength() > 0) {
+                Node dutyTime7sNode = dutyTime7sList.item(0);
+                pharmacyDto.setDutyTime7s(dutyTime7sNode.getTextContent().trim());
+            }
 
-                NodeList dutyTime8cList = ((Element) itemNode).getElementsByTagName("dutyTime8c");
-                if (dutyTime8cList.getLength() > 0) {
-                    Node dutyTime8cNode = dutyTime8cList.item(0);
-                    pharmacyDto.setDutyTime8c(null);
-                }
+            NodeList dutyTime8cList = ((Element) itemNode).getElementsByTagName("dutyTime8c");
+            if (dutyTime8cList.getLength() > 0) {
+                Node dutyTime8cNode = dutyTime8cList.item(0);
+                pharmacyDto.setDutyTime8c(dutyTime8cNode.getTextContent().trim());
+            }
 
-                NodeList dutyTime8sList = ((Element) itemNode).getElementsByTagName("dutyTime8s");
-                if (dutyTime8sList.getLength() > 0) {
-                    Node dutyTime8sNode = dutyTime8sList.item(0);
-                    pharmacyDto.setDutyTime8s(null);
-                }
+            NodeList dutyTime8sList = ((Element) itemNode).getElementsByTagName("dutyTime8s");
+            if (dutyTime8sList.getLength() > 0) {
+                Node dutyTime8sNode = dutyTime8sList.item(0);
+                pharmacyDto.setDutyTime8s(dutyTime8sNode.getTextContent().trim());
             }
 
 
@@ -211,101 +208,99 @@ public class XMLParser {
         NodeList dutyTime1cList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime1c");
         if (dutyTime1cList.getLength() > 0) {
             Node dutyTime1cNode = dutyTime1cList.item(0);
-            pharmacy.setDutyTime1c(Integer.parseInt(dutyTime1cNode.getTextContent().trim()));
+            pharmacy.setDutyTime1c(dutyTime1cNode.getTextContent().trim());
         }
 
         NodeList dutyTime1sList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime1s");
         if (dutyTime1sList.getLength() > 0) {
             Node dutyTime1sNode = dutyTime1sList.item(0);
-            pharmacy.setDutyTime1s(Integer.parseInt(dutyTime1sNode.getTextContent().trim()));
+            pharmacy.setDutyTime1s(dutyTime1sNode.getTextContent().trim());
         }
 
         NodeList dutyTime2cList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime2c");
         if (dutyTime2cList.getLength() > 0) {
             Node dutyTime2cNode = dutyTime2cList.item(0);
-            pharmacy.setDutyTime2c(Integer.parseInt(dutyTime2cNode.getTextContent().trim()));
+            pharmacy.setDutyTime2c(dutyTime2cNode.getTextContent().trim());
         }
 
         NodeList dutyTime2sList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime2s");
         if (dutyTime2sList.getLength() > 0) {
             Node dutyTime2sNode = dutyTime2sList.item(0);
-            pharmacy.setDutyTime2s(Integer.parseInt(dutyTime2sNode.getTextContent().trim()));
+            pharmacy.setDutyTime2s(dutyTime2sNode.getTextContent().trim());
         }
 
         NodeList dutyTime3cList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime3c");
         if (dutyTime3cList.getLength() > 0) {
             Node dutyTime3cNode = dutyTime3cList.item(0);
-            pharmacy.setDutyTime3c(Integer.parseInt(dutyTime3cNode.getTextContent().trim()));
+            pharmacy.setDutyTime3c(dutyTime3cNode.getTextContent().trim());
         }
 
         NodeList dutyTime3sList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime3s");
         if (dutyTime3sList.getLength() > 0) {
             Node dutyTime3sNode = dutyTime3sList.item(0);
-            pharmacy.setDutyTime3s(Integer.parseInt(dutyTime3sNode.getTextContent().trim()));
+            pharmacy.setDutyTime3s(dutyTime3sNode.getTextContent().trim());
         }
 
         NodeList dutyTime4cList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime4c");
         if (dutyTime4cList.getLength() > 0) {
             Node dutyTime4cNode = dutyTime4cList.item(0);
-            pharmacy.setDutyTime4c(Integer.parseInt(dutyTime4cNode.getTextContent().trim()));
+            pharmacy.setDutyTime4c(dutyTime4cNode.getTextContent().trim());
         }
 
         NodeList dutyTime4sList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime4s");
         if (dutyTime4sList.getLength() > 0) {
             Node dutyTime4sNode = dutyTime4sList.item(0);
-            pharmacy.setDutyTime4s(Integer.parseInt(dutyTime4sNode.getTextContent().trim()));
+            pharmacy.setDutyTime4s(dutyTime4sNode.getTextContent().trim());
         }
 
         NodeList dutyTime5cList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime5c");
         if (dutyTime5cList.getLength() > 0) {
             Node dutyTime5cNode = dutyTime5cList.item(0);
-            pharmacy.setDutyTime5c(Integer.parseInt(dutyTime5cNode.getTextContent().trim()));
+            pharmacy.setDutyTime5c(dutyTime5cNode.getTextContent().trim());
         }
 
         NodeList dutyTime5sList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime5s");
         if (dutyTime5sList.getLength() > 0) {
             Node dutyTime5sNode = dutyTime5sList.item(0);
-            pharmacy.setDutyTime5s(Integer.parseInt(dutyTime5sNode.getTextContent().trim()));
+            pharmacy.setDutyTime5s(dutyTime5sNode.getTextContent().trim());
         }
 
         NodeList dutyTime6cList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime6c");
         if (dutyTime6cList.getLength() > 0) {
             Node dutyTime6cNode = dutyTime6cList.item(0);
-            pharmacy.setDutyTime6c(Integer.parseInt(dutyTime6cNode.getTextContent().trim()));
+            pharmacy.setDutyTime6c(dutyTime6cNode.getTextContent().trim());
         }
 
         NodeList dutyTime6sList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime6s");
         if (dutyTime6sList.getLength() > 0) {
             Node dutyTime6sNode = dutyTime6sList.item(0);
-            pharmacy.setDutyTime6s(Integer.parseInt(dutyTime6sNode.getTextContent().trim()));
+            pharmacy.setDutyTime6s(dutyTime6sNode.getTextContent().trim());
         }
 
-        // 7,8이 있을때만 작동
-        if (dutyTime6sList.getLength() > 0) {
-            NodeList dutyTime7cList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime7c");
-            if (dutyTime7cList.getLength() > 0) {
-                Node dutyTime7cNode = dutyTime7cList.item(0);
-                pharmacy.setDutyTime7c(null);
-            }
-
-            NodeList dutyTime7sList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime7s");
-            if (dutyTime7sList.getLength() > 0) {
-                Node dutyTime7sNode = dutyTime7sList.item(0);
-                pharmacy.setDutyTime7s(null);
-            }
-
-            NodeList dutyTime8cList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime8c");
-            if (dutyTime8cList.getLength() > 0) {
-                Node dutyTime8cNode = dutyTime8cList.item(0);
-                pharmacy.setDutyTime8c(null);
-            }
-
-            NodeList dutyTime8sList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime8s");
-            if (dutyTime8sList.getLength() > 0) {
-                Node dutyTime8sNode = dutyTime8sList.item(0);
-                pharmacy.setDutyTime8s(null);
-            }
+        NodeList dutyTime7cList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime7c");
+        if (dutyTime7cList.getLength() > 0) {
+            Node dutyTime7cNode = dutyTime7cList.item(0);
+            pharmacy.setDutyTime7c(dutyTime7cNode.getTextContent().trim());
         }
+
+        NodeList dutyTime7sList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime7s");
+        if (dutyTime7sList.getLength() > 0) {
+            Node dutyTime7sNode = dutyTime7sList.item(0);
+            pharmacy.setDutyTime7s(dutyTime7sNode.getTextContent().trim());
+        }
+
+        NodeList dutyTime8cList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime8c");
+        if (dutyTime8cList.getLength() > 0) {
+            Node dutyTime8cNode = dutyTime8cList.item(0);
+            pharmacy.setDutyTime8c(dutyTime8cNode.getTextContent().trim());
+        }
+
+        NodeList dutyTime8sList = ((Element) nodeList.item(0)).getElementsByTagName("dutyTime8s");
+        if (dutyTime8sList.getLength() > 0) {
+            Node dutyTime8sNode = dutyTime8sList.item(0);
+            pharmacy.setDutyTime8s(dutyTime8sNode.getTextContent().trim());
+        }
+
 
         NodeList hpidList = ((Element) nodeList.item(0)).getElementsByTagName("hpid");
         if (hpidList.getLength() > 0) {
