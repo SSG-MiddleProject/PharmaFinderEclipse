bd5a1dfae6d951330a962916a1018358eb5b4a32
1min <dnjsxmf@naver.com>
2024-02-14 11:19:07 +0900

    feat: 재고관리 기능 최초 커밋

---
+++ src/main/java/ssg/middlepj/pharmafinder/controller/PharmaInventoryController.java
@@ -0,0 +1,60 @@
+package ssg.middlepj.pharmafinder.controller;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import ssg.middlepj.pharmafinder.dto.InventoryDto;
+import ssg.middlepj.pharmafinder.dto.PharmaInventoryParam;
+import ssg.middlepj.pharmafinder.service.PharmaInventoryService;
+import util.PharmaInOutCalendarUtil;
+
+@Controller
+public class PharmaInventoryController {
+
+	@Autowired
+	private PharmaInventoryService service;
+
+	// 처음 진입점
+	@GetMapping(value = "/pharma-inventory-inquiry.do")
+	public String pharmaInventoryInquiry(String searchType, String keyword, PharmaInventoryParam param, Model model, HttpServletRequest request) {
+		System.out.println("pharmaInOutCalendar pharmaInventoryInquiry()");
+
+		if((searchType !=  null && searchType.equals("id")) && (keyword != null && !keyword.equals(""))) {
+			param.setProductId(Integer.parseInt(keyword));
+			model.addAttribute("searchType", searchType);
+			model.addAttribute("keyword", keyword);
+		} else if((searchType !=  null && searchType.equals("itemName")) && (keyword != null && !keyword.equals(""))) {
+			param.setItemName(keyword);
+			model.addAttribute("searchType", searchType);
+			model.addAttribute("keyword", keyword);
+		}
+
+		String currentDate;
+
+		if (param.getCurrentDate() == null || param.getCurrentDate() == "") {
+			LocalDate currentDateObj = LocalDate.now();
+			currentDate = currentDateObj.toString();
+		} else {
+			currentDate = param.getCurrentDate();
+		}
+
+		int storeId = (int) request.getSession().getAttribute("storeId");
+
+		param.setCurrentDate(currentDate);
+		param.setStoreId(storeId);
+
+		List<InventoryDto> list = service.selectInventory(param);
+
+		model.addAttribute("currentDate", currentDate);
+		model.addAttribute("list", list);
+
+		return "pharma/inventory/inquiry.tiles";
+	}
+}
